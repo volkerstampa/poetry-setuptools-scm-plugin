afe6344b8ed3e7fd474b51fa084bd5e42509bce3
Volker Stampa <Volker.Stampa@gmx.net>
2024-04-20 19:17:56 +0200

    Publish only from main

--- publish.sh
+++ publish.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 
 PypiRepository="$1"
+if [[ "${2-}" = "--build-only" ]]
+then
+  BuildOnly=true
+else
+  BuildOnly=false
+fi
 
 poetry self add poetry-setuptools-scm-plugin
 if [[ "$PypiRepository" == "testpypi" ]]
 then
   poetry config repositories.testpypi https://test.pypi.org/legacy/
 fi
-poetry config pypi-token."$PypiRepository" "$PYPI_TOKEN"
+poetry config pypi-token."$PypiRepository" "${PYPI_TOKEN}"
+poetry run poetry build
+"$BuildOnly" && exit 0
 if [[ "$PypiRepository" == "testpypi" ]]
 then
-  poetry run poetry --no-interaction publish --build --repository "$PypiRepository"
+  poetry run poetry --no-interaction publish --repository "$PypiRepository"
 else
-  poetry run poetry --no-interaction publish  --build
+  poetry run poetry --no-interaction publish
 fi
